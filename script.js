3a8ae19db1ecdd7c6d273bd82df3c6baa33992c7
anujkumarsingh1999 <98317240+anujkumarsingh1999@users.noreply.github.com>
2022-08-17 19:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["UI/UX Designer", "YouTuber", "Freelancer", "Industrial Engineer"],
+        strings: ["Product Designer","UI/UX Designer", "YouTuber", "Freelancer", "Industrial Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
